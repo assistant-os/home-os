6d7f4673dc1d7b35694845f439a1dad54108322e
Thibault Friedrich <thibault.friedrich@gmail.com>
2021-03-01 17:03:39 +0000

    feat: add person detection

--- camera/src/camera-stream.py
+++ camera/src/camera-stream.py
@@ -11,10 +11,24 @@ import cv2
 import os
 import numpy as np 
 
+dirname = os.path.dirname(__file__)
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(
+		os.path.join(dirname, '../models/MobileNetSSD_deploy.prototxt'),
+		os.path.join(dirname, '../models/MobileNetSSD_deploy.caffemodel'))
+
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+RED = (255, 0, 0)
+
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful when multiple browsers/tabs
 # are viewing the stream)
-frame = None
+globalFrame = None
 lock = threading.Lock()
 # initialize a flask object
 app = Flask(__name__)
@@ -25,6 +39,37 @@ vs = VideoStream(usePiCamera=0).start()
 
 time.sleep(2.0)
 
+def mat2gray(img):
+		A = np.double(img)
+		out = np.zeros(A.shape, np.double)
+		normalized = cv2.normalize(A, out, 1.0, 0.0, cv2.NORM_MINMAX)
+		return out
+
+ # Add noise to the image
+
+
+def random_noise(image, mode='gaussian', seed=None, clip=True, **kwargs):
+		image = mat2gray(image)
+
+		mode = mode.lower()
+		if image.min() < 0:
+				low_clip = -1
+		else:
+				low_clip = 0
+		if seed is not None:
+				np.random.seed(seed=seed)
+
+		if mode == 'gaussian':
+				noise = np.random.normal(kwargs['mean'], kwargs['var'] ** 0.5,
+																 image.shape)
+				out = image + noise
+		if clip:
+				out = np.clip(out, low_clip, 1.0)
+
+		return out
+
+
+
 @app.route("/")
 def index():
 	# return the rendered template
@@ -33,15 +78,58 @@ def index():
 def detect_motion(frameCount, rotate):
 	# grab global references to the video stream, output frame, and
 	# lock variables
-	global vs, frame, lock
+	global vs, globalFrame, lock
 
-  # loop over frames from the video stream
+	# loop over frames from the video stream
 	while True:
 		# read the next frame from the video stream, resize it,
 		# convert the frame to grayscale, and blur it
-		badFrame = vs.read()
+		frame = vs.read()
 
-		frame = np.array(np.rot90(badFrame, k=2))
+		frame = np.array(np.rot90(frame, k=2))
+		frame = imutils.resize(frame, width=512)
+
+		(h, w) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (512, 512)), 0.007843, (512, 512), 127.5)
+
+		# pass the blob through the network and obtain the detections and predictions
+		net.setInput(blob)
+		detections = net.forward()
+
+		nbPerson = 0
+		confidences = []
+
+		# loop over the detections
+		for i in np.arange(0, detections.shape[2]):
+				# extract the confidence (i.e., probability) associated with
+				# the prediction
+				confidence = detections[0, 0, i, 2]
+
+				# filter out weak detections by ensuring the `confidence` is
+				# greater than the minimum confidence
+				if confidence > args["confidence"]:
+						# extract the index of the class label from the
+						# `detections`, then compute the (x, y)-coordinates of
+						# the bounding box for the object
+						idx = int(detections[0, 0, i, 1])
+						box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+						(startX, startY, endX, endY) = box.astype("int")
+
+						if CLASSES[idx] == "person":
+								cv2.rectangle(frame, (startX, startY), (endX, endY),
+															RED, 2)
+								nbPerson += 1
+								label = "{:.2f}%".format(confidence * 100)
+								y = startY - 15 if startY - 15 > 15 else startY + 15
+								cv2.putText(frame, label, (startX, y),
+														cv2.FONT_HERSHEY_SIMPLEX, 0.5, RED, 2)
+
+		label = "{}".format(nbPerson)
+		cv2.putText(frame, label, (10, 20),
+								cv2.FONT_HERSHEY_SIMPLEX, 0.5, RED, 2)
+
+		globalFrame = frame
+
 		# grab the current timestamp and draw it on the frame
 		timestamp = datetime.datetime.now()
 		cv2.putText(frame, timestamp.strftime(
@@ -50,17 +138,17 @@ def detect_motion(frameCount, rotate):
 
 def generate():
 	# grab global references to the output frame and lock variables
-	global frame, lock
+	global globalFrame, lock
 	# loop over frames from the output stream
 	while True:
 		# wait until the lock is acquired
 		with lock:
 			# check if the output frame is available, otherwise skip
 			# the iteration of the loop
-			if frame is None:
+			if globalFrame is None:
 				continue
 			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", frame)
+			(flag, encodedImage) = cv2.imencode(".jpg", globalFrame)
 			# ensure the frame was successfully encoded
 			if not flag:
 				continue
@@ -82,8 +170,8 @@ def save():
 	# return the response generated along with the specific media
 	# type (mime type)
 	return Response("{}",
-        status=200,
-        mimetype='application/json')
+				status=200,
+				mimetype='application/json')
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
@@ -97,6 +185,8 @@ if __name__ == '__main__':
 		help="angle of rotation of the image")
 	ap.add_argument("-f", "--frame-count", type=int, default=32,
 		help="# of frames used to construct the background model")
+	ap.add_argument('-c', '--confidence', type=float, default=0.5,
+                help='minimum probability to filter weak detections')
 	args = vars(ap.parse_args())
 	# start a thread that will perform motion detection
 	t = threading.Thread(target=detect_motion, args=(
